13a46120ea492e5c93ad1fd3fb79c0afd035c082
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-12-14 14:09:19 +0100

    Proyecto spring boot con Dockerfile tres versiones

---
+++ EjemploDocker/src/main/java/com/salesianostriana/dam/EjemploDocker/EjemploDockerApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.EjemploDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploDockerApplication.class, args);
+	}
+
+}
---
+++ EjemploDocker/src/test/java/com/salesianostriana/dam/EjemploDocker/EjemploDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.EjemploDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
